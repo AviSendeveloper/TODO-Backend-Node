78943af056e749046ecae56af364ce3d089de7fa
AvijitSen <senavijit1697@gmail.com>
2023-06-27 00:26:47 +0530

    update and delete operation complete, add sort on fetch all

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -4,7 +4,7 @@ const Task = require("../models/Task");
 exports.list = async (req, res, next) => {
     let status, statusCode, data;
     try {
-        const result = await Task.find({});
+        const result = await Task.find({}).sort({createdAt: -1});
         status = true;
         statusCode = 200;
         data = result;
@@ -49,4 +49,32 @@ exports.create = async (req, res, next) => {
         status: status,
         data: data,
     });
+};
+
+exports.update = async (req, res, next) => {
+    const { id: _id, title, date } = req.body;
+    const result = await Task.updateOne(
+        { _id: _id },
+        { title: title, date: date }
+    );
+    return res.status(200).json({
+        status: true,
+        data: result
+    })
+};
+
+exports.deleteList = async (req, res, next) => {
+    try {
+        const _id = req.body.id;
+        const result = await Task.deleteOne({ _id: _id });
+        return res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            data: error,
+        });
+    }
 };
--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -4,5 +4,7 @@ const TaskCreateValidation = require("../validations/TaskCreateValidation");
 
 Router.get("/list", TaskController.list);
 Router.post("/create", TaskCreateValidation, TaskController.create);
+Router.post("/update", TaskController.update);
+Router.post("/delete", TaskController.deleteList);
 
 module.exports = Router;
