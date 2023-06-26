0a4c7945618809e8ba1a58027e0257829f5265ad
Avijit Sen <avijit.sen@tier5.in>
2023-06-26 15:08:14 +0530

    add cors package to reslove cors issue

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const connectDB = require('./database/connection');
-const TaskRoute = require('./routes/TaskRoute');
+const cors = require("cors");
+const connectDB = require("./database/connection");
+const TaskRoute = require("./routes/TaskRoute");
 
 const app = express();
 
+app.use(
+    cors({
+        origin: "*",
+        methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/task', TaskRoute);
+app.use("/task", TaskRoute);
 
-app.use('/', (req, res, next) => {
+app.use("/", (req, res, next) => {
     res.status(404).json({
-        'status': false,
-        'message': 'Not found'
-    })
-})
+        status: false,
+        message: "Not found",
+    });
+});
 
 connectDB();
 app.listen(3000);
