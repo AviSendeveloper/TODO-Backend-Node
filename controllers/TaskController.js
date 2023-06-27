75955161c9ae0b9c345017d9dacbda22582a0e55
Avijit Sen <avijit.sen@tier5.in>
2023-06-27 17:26:22 +0530

    fix issue for data to date

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -4,7 +4,27 @@ const Task = require("../models/Task");
 exports.list = async (req, res, next) => {
     let status, statusCode, data;
     try {
-        const result = await Task.find({}).sort({createdAt: -1});
+        const result = await Task.find({}).sort({ createdAt: -1 });
+        status = true;
+        statusCode = 200;
+        data = result;
+    } catch (error) {
+        status = false;
+        statusCode = 500;
+        data = null;
+    }
+
+    return res.status(statusCode).json({
+        status: status,
+        data: data,
+    });
+};
+
+exports.find = async (req, res, next) => {
+    const _id = req.query.id;
+    let status, statusCode, data;
+    try {
+        const result = await Task.findOne({ _id: _id });
         status = true;
         statusCode = 200;
         data = result;
@@ -34,7 +54,7 @@ exports.create = async (req, res, next) => {
         const { title, date } = req.body;
         const response = await Task.create({
             title: title,
-            dueData: date,
+            dueDate: new Date(date),
         });
         status = true;
         statusCode = 200;
@@ -52,15 +72,24 @@ exports.create = async (req, res, next) => {
 };
 
 exports.update = async (req, res, next) => {
-    const { id: _id, title, date } = req.body;
-    const result = await Task.updateOne(
-        { _id: _id },
-        { title: title, date: date }
-    );
-    return res.status(200).json({
-        status: true,
-        data: result
-    })
+    try {
+        const { id: _id, title, date } = req.body;
+        const result = await Task.findOneAndUpdate(
+            { _id: _id },
+            { $set: { title: title, dueDate: new Date(date) } },
+            { new: true }
+        );
+        return res.status(200).json({
+            status: true,
+            data: result,
+        });
+    } catch (error) {
+        return res.status(200).json({
+            status: false,
+            data: null,
+            error: JSON.stringify(error),
+        });
+    }
 };
 
 exports.deleteList = async (req, res, next) => {
--- routes/TaskRoute.js
+++ routes/TaskRoute.js
@@ -3,6 +3,7 @@ const TaskController = require("../controllers/TaskController");
 const TaskCreateValidation = require("../validations/TaskCreateValidation");
 
 Router.get("/list", TaskController.list);
+Router.get("/find", TaskController.find);
 Router.post("/create", TaskCreateValidation, TaskController.create);
 Router.post("/update", TaskController.update);
 Router.post("/delete", TaskController.deleteList);
